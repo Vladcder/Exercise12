5e215a4ae9afd34d0d725012747a29226d6ad8cf
Vlad <gresko.97@mail.ru>
2024-01-11 15:20:14 +0300

    Add project files.

---
+++ Task1/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class Circle
+    {
+
+        public static double CalculateLength(double radius)
+        {
+            return 2*Math.PI*radius;
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * radius*radius;
+        }
+
+        public static bool IsPointInCircle(double x, double y, double r, double x0, double y0)
+        {    
+            return Math.Sqrt((x0 - x) * (x0 - x) + (y0 - y) * (y0 - y)) <= r;
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение радиуса круга: ");
+            double radius=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Длина окружности равна "+ Circle.CalculateLength(radius));
+            Console.WriteLine("Площадь окружности равна " + Circle.CalculateArea(radius));
+            Console.Write("Введите координату x0 центра окружности ");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату y0 центра окружности ");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату точки x ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату точки y ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            if (Circle.IsPointInCircle(x,y,radius,x0,y0))
+            {
+                Console.WriteLine($"Точка ({x},{y}) лежит в окружности с радиусом {radius} и центром в точке ({x0},{y0})");
+            }
+            else
+            {
+                Console.WriteLine($"Точка ({x},{y}) не лежит в окружности с радиусом {radius} и центром в точке ({x0},{y0})");
+
+            }
+        }
+    }
+}
